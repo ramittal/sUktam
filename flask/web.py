55f0a48b70054549aaa909adb17ad8e4a9f2c3cf
Krish Sridhar <50347758+KrishTheDuck@users.noreply.github.com>
2022-12-28 21:48:02 -0800

    Update web.py
    added random phrase capability

--- flask/web.py
+++ flask/web.py
@@ -1,10 +1,12 @@
+import random
+
 from flask import Flask
-from flask import flash
 from flask import redirect
 from flask import render_template
 from flask import request
 
 from flask_session import Session
+from pathlib import Path
 
 UPLOAD_FOLDER = 'files'
 app = Flask(__name__)
@@ -16,35 +18,44 @@ app.config['SESSION_TYPE'] = 'filesystem'
 sess.init_app(app)
 
 
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    return '<h1></h1>'
+
+
+@app.route('/get_random_line', methods=['GET'])
+def get_random_line():
+    path = Path(__file__).parent.parent.__str__() + "\\reference_files.txt"
+    lines = open(path, encoding="UTF-8").readlines()[1:]
+    return random.choice(lines)
+
+
 @app.route('/')
 def root():
     return render_template('record.html', name=None)
 
 
-@app.route('/favicon.ico', methods=['GET'])
-def favicon():
-    return "<h1></h1>"
-
-
 @app.route('/save-record', methods=['GET', 'POST'])
 def save_record():
     if request.method == "GET":
-        return app.secret_key
+        return ""
 
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash("what happened to the file?")
-        redirect(request.url)
+    if request.method == "POST":
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print("what happened to the file?")
+            redirect(request.url)
 
-    file = request.files['file']
+        file = request.files['file']
 
-    if file.filename == '':
-        flash("what happened to the filename?")
-        redirect(request.url)
+        if file.filename == '':
+            print("what happened to the filename?")
+            redirect(request.url)
 
-    filename = "testfile.mp3"
-    file.save("files/"+filename)
-    return redirect(request.url)
+        path = Path(__file__).parent.__str__() + "\\files\\testfile.mp3";
+        file.save(path)
+
+        return redirect(request.url)
 
 
 def run():
